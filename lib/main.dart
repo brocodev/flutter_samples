06ca0a361f46fc9774731d9e27aa63455359d3d7
Kevin Melendez <kevinmdezhdez@gmail.com>
2023-08-05 22:56:49 -0600

    🎨 style: Changed the color of the status bar to transparent

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_samples/core/app/app.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
   runApp(const SamplesApp());
 }
